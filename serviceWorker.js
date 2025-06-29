057b4815982a390fd2ad7a16a8c3120dfec41262
ZR1389 <87881221+ZR1389@users.noreply.github.com>
2025-06-29 10:13:06 -0700

    Add files via upload

---
+++ serviceWorker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'sentinel-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
